f07a62b274809b177725ffa3e592deb2960754a2
Deji Sanusi <dsanusi90@gmail.com>
2020-12-08 10:15:13 -0500

    third commit

--- myreactblog/src/pages/ArticlePage.js
+++ myreactblog/src/pages/ArticlePage.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import articleContent from './article-content'
 import ArticlesList from "../components/ArticlesList/ArticlesList"
 import NotFoundPage from "./NotFoundPage"
-const ArticlePage  = ({ match}) => {
-    const name = match.params.name
-    const article = articleContent.find(article => article.name === name)
+import CommentsList from "../components/CommentsList/CommentsList"
+import UpvotesSection from "../components/UpvotesSection/UpvotesSection"
+import AddCommentForm from "../components/AddCommentForm/AddCommentForm"
 
-    if (!article) return <NotFoundPage/>
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = articleContent.find(article => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
     return(
     <>
     <h1>{article.title}</h1>
+    <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo}/>
     {article.content.map((paragraph, key) => (
         <p key = {key}>{paragraph}</p>
 
     ))}
+    <CommentsList comments={articleInfo.comments}/>
+    <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
     <h3>Other Articles</h3>
     <ArticlesList articles={otherArticles}/>
     </>
